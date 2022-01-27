664b8e7c76eaa357148fd685891a671d4e61e3c3
Callum Mackenzie <Alexxander1611@gmail.com>
2022-01-27 11:35:50 -0700

    Add sex as part of body statistics

--- src/main/java/com/camackenzie/exvi/core/model/BodyStats.java
+++ src/main/java/com/camackenzie/exvi/core/model/BodyStats.java
@@ -13,10 +13,22 @@ public class BodyStats {
 
     private UnitValue<MassUnit> weight;
     private UnitValue<DistanceUnit> height;
+    private GeneticSex sex;
 
-    public BodyStats(UnitValue<MassUnit> weight, UnitValue<DistanceUnit> height) {
+    public BodyStats(GeneticSex sex,
+            UnitValue<MassUnit> weight,
+            UnitValue<DistanceUnit> height) {
         this.weight = weight;
         this.height = height;
+        this.sex = sex;
+    }
+
+    public GeneticSex getSex() {
+        return this.sex;
+    }
+
+    public void setGeneticSex(GeneticSex sex) {
+        this.sex = sex;
     }
 
     public UnitValue<MassUnit> getTotalMass() {
@@ -36,17 +48,20 @@ public class BodyStats {
     }
 
     public static BodyStats averageMale() {
-        return new BodyStats(new UnitValue(MassUnit.POUND, 190),
+        return new BodyStats(GeneticSex.MALE,
+                new UnitValue(MassUnit.POUND, 190),
                 new UnitValue(DistanceUnit.METER, 1.7));
     }
 
     public static BodyStats averageFemale() {
-        return new BodyStats(new UnitValue(MassUnit.POUND, 170),
+        return new BodyStats(GeneticSex.FEMALE,
+                new UnitValue(MassUnit.POUND, 170),
                 new UnitValue(DistanceUnit.METER, 1.625));
     }
 
     public static BodyStats average() {
-        return new BodyStats(new UnitValue(MassUnit.POUND, 180),
+        return new BodyStats(GeneticSex.UNKNOWN,
+                new UnitValue(MassUnit.POUND, 180),
                 new UnitValue(DistanceUnit.METER, 1.68));
     }
 
---
+++ src/main/java/com/camackenzie/exvi/core/model/GeneticSex.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.camackenzie.exvi.core.model;
+
+/**
+ *
+ * @author callum
+ */
+public enum GeneticSex {
+    MALE,
+    FEMALE,
+    UNKNOWN
+}
