2f619352fdcd36b51b9864e0f11a9bf63506d814
Callum Mackenzie <Alexxander1611@gmail.com>
2022-02-04 18:38:10 -0700

    Opening whole module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-module com.camackenzie.exvi.core {
+open module com.camackenzie.exvi.core {
     requires com.google.gson;
     requires java.net.http;
 
@@ -12,9 +12,4 @@ module com.camackenzie.exvi.core {
     exports com.camackenzie.exvi.core.async;
     exports com.camackenzie.exvi.core.model;
     exports com.camackenzie.exvi.core.util;
-
-    opens com.camackenzie.exvi.core.model;
-    opens com.camackenzie.exvi.core.api;
-    opens com.camackenzie.exvi.core.async;
-    opens com.camackenzie.exvi.core.util;
 }
