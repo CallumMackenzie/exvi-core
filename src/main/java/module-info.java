89116ac99fb785f9cc8052bbdb4805ef86760c7c
Callum Mackenzie <Alexxander1611@gmail.com>
2022-02-04 18:17:57 -0700

    Update module name and exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,12 @@
  * and open the template in the editor.
  */
 
-module ExviCore {
+module com.camackenzie.exvi.core {
     requires com.google.gson;
     requires java.net.http;
+
     exports com.camackenzie.exvi.core.api;
     exports com.camackenzie.exvi.core.async;
+    exports com.camackenzie.exvi.core.model;
     exports com.camackenzie.exvi.core.util;
 }
