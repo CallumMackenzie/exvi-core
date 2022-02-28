6a74cf51dd19b377d59d038a304eae4e2ddcf550
Callum Mackenzie <Alexxander1611@gmail.com>
2022-02-27 17:01:31 -0700

    Add compound assignment operator overloads

--- exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/UnitValue.kt
+++ exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/UnitValue.kt
@@ -10,34 +10,67 @@ package com.camackenzie.exvi.core.model
  * @author callum
  */
 @kotlinx.serialization.Serializable
-data class UnitValue<T : Unit>(
-    val unit: T,
-    val value: Double
+class UnitValue<T : Unit>(
+    private var iUnit: T,
+    private var iValue: Double
 ) {
+    val unit
+        get() = iUnit
+    val value
+        get() = iValue
+
+    private fun toOtherValue(unit: T): Double = iValue / iUnit.getBaseCoefficient() * unit.getBaseCoefficient()
+
+    /**
+     * Returns a new UnitValue with this unit converted to the given unit
+     * Does not modify this object
+     */
     fun toUnit(unit: T): UnitValue<T> {
-        return if (unit == this.unit) this
-        else UnitValue(unit, this.value / this.unit.getBaseCoefficient() * unit.getBaseCoefficient())
+        return if (unit == iUnit) this
+        else UnitValue(unit, toOtherValue(unit))
+    }
+
+    /**
+     * Converts this unit value to the other and returns this object
+     * Modifies this object
+     */
+    fun asUnit(unit: T): UnitValue<T> {
+        if (unit == iUnit) this
+        else {
+            iValue = toOtherValue(unit)
+            iUnit = unit
+        }
+        return this
     }
 
     private inline fun applyToValue(other: UnitValue<T>, apply: (Double) -> Double): UnitValue<T> =
-        UnitValue(unit, apply(other.toUnit(unit).value))
+        UnitValue(iUnit, apply(other.toUnit(iUnit).iValue))
 
-    operator fun plus(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { value + it }
-    operator fun minus(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { value - it }
-    operator fun times(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { value * it }
-    operator fun div(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { value / it }
-    operator fun unaryMinus(): UnitValue<T> = UnitValue(unit, -value)
-    operator fun unaryPlus(): UnitValue<T> = UnitValue(unit, +value)
+    private inline fun applyAsValue(other: UnitValue<T>, apply: (Double) -> kotlin.Unit) =
+        apply(other.toOtherValue(iUnit))
+
+    operator fun plus(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue + it }
+    operator fun minus(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue - it }
+    operator fun times(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue * it }
+    operator fun div(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue / it }
+
+    operator fun plusAssign(other: UnitValue<T>) = applyAsValue(other) { iValue += it }
+    operator fun minusAssign(other: UnitValue<T>) = applyAsValue(other) { iValue -= it }
+    operator fun timesAssign(other: UnitValue<T>) = applyAsValue(other) { iValue *= it }
+    operator fun divAssign(other: UnitValue<T>) = applyAsValue(other) { iValue /= it }
+
+    operator fun unaryMinus(): UnitValue<T> = UnitValue(iUnit, -iValue)
+    operator fun unaryPlus(): UnitValue<T> = UnitValue(iUnit, +iValue)
 
     override fun equals(other: Any?): Boolean {
         return if (other is UnitValue<*>) {
-            if (this.unit == other.unit)
-                this.value == other.value
-            else if (this.unit::class == other.unit::class)
-                this.toUnit(other.unit as T) == other
+            if (this.iUnit == other.iUnit)
+                this.iValue == other.iValue
+            else if (this.iUnit::class == other.iUnit::class)
+                this.toUnit(other.iUnit as T) == other
             else false
         } else false
     }
 
-    override fun toString(): String = "$value ${unit.toString().lowercase()}s"
+    override fun toString(): String = "$iValue ${iUnit.toString().lowercase()}s"
 }
