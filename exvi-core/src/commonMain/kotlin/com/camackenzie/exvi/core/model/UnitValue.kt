45090c4d1c2a2aacd02c1198b9287412df8f1fff
Callum Mackenzie <Alexxander1611@gmail.com>
2022-02-27 17:08:20 -0700

    Integer & Double operator overloads for the UnitValue class

--- exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/UnitValue.kt
+++ exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/UnitValue.kt
@@ -10,7 +10,7 @@ package com.camackenzie.exvi.core.model
  * @author callum
  */
 @kotlinx.serialization.Serializable
-class UnitValue<T : Unit>(
+class UnitValue<T : Unit> private constructor(
     private var iUnit: T,
     private var iValue: Double
 ) {
@@ -49,16 +49,65 @@ class UnitValue<T : Unit>(
     private inline fun applyAsValue(other: UnitValue<T>, apply: (Double) -> kotlin.Unit) =
         apply(other.toOtherValue(iUnit))
 
+    // Unit binary operators
     operator fun plus(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue + it }
     operator fun minus(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue - it }
     operator fun times(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue * it }
     operator fun div(other: UnitValue<T>): UnitValue<T> = applyToValue(other) { iValue / it }
 
+    // Integer binary operators
+    operator fun plus(i: Int): UnitValue<T> = UnitValue(iUnit, iValue + i)
+    operator fun minus(i: Int): UnitValue<T> = UnitValue(iUnit, iValue - i)
+    operator fun times(i: Int): UnitValue<T> = UnitValue(iUnit, iValue * i)
+    operator fun div(i: Int): UnitValue<T> = UnitValue(iUnit, iValue / i)
+
+    // Double binary operators
+    operator fun plus(i: Double): UnitValue<T> = UnitValue(iUnit, iValue + i)
+    operator fun minus(i: Double): UnitValue<T> = UnitValue(iUnit, iValue - i)
+    operator fun times(i: Double): UnitValue<T> = UnitValue(iUnit, iValue * i)
+    operator fun div(i: Double): UnitValue<T> = UnitValue(iUnit, iValue / i)
+
+    // Unit assignment operators
     operator fun plusAssign(other: UnitValue<T>) = applyAsValue(other) { iValue += it }
     operator fun minusAssign(other: UnitValue<T>) = applyAsValue(other) { iValue -= it }
     operator fun timesAssign(other: UnitValue<T>) = applyAsValue(other) { iValue *= it }
     operator fun divAssign(other: UnitValue<T>) = applyAsValue(other) { iValue /= it }
 
+    // Integer assignment operators
+    operator fun plusAssign(i: Int) {
+        iValue += i
+    }
+
+    operator fun minusAssign(i: Int) {
+        iValue -= i
+    }
+
+    operator fun timesAssign(i: Int) {
+        iValue *= i
+    }
+
+    operator fun divAssign(i: Int) {
+        iValue /= i
+    }
+
+    // Double assignment operators
+    operator fun plusAssign(i: Double) {
+        iValue += i
+    }
+
+    operator fun minusAssign(i: Double) {
+        iValue -= i
+    }
+
+    operator fun timesAssign(i: Double) {
+        iValue *= i
+    }
+
+    operator fun divAssign(i: Double) {
+        iValue /= i
+    }
+
+    // Unary operators
     operator fun unaryMinus(): UnitValue<T> = UnitValue(iUnit, -iValue)
     operator fun unaryPlus(): UnitValue<T> = UnitValue(iUnit, +iValue)
 
