5a290c84e32d4d30bbdf395bb4a6db6b0c9889c9
Callum Mackenzie <Alexxander1611@gmail.com>
2022-04-05 21:58:46 -0600

    Update APIRequest constructors with JvmOverloads annotations

--- exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/api/APIRequest.kt
+++ exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/api/APIRequest.kt
@@ -17,6 +17,7 @@ import io.ktor.utils.io.core.*
 import kotlinx.coroutines.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import kotlin.jvm.JvmOverloads
 import kotlin.jvm.JvmStatic
 
 /**
@@ -36,12 +37,14 @@ class APIRequest<T : SelfSerializable> {
     @Transient
     var endpoint: String = ""
 
+    @JvmOverloads
     constructor(endpoint: String, body: T, headers: HashMap<String, String> = HashMap()) {
         this.endpoint = endpoint
         this.body = body
         this.headers = headers
     }
 
+    @JvmOverloads
     constructor(body: T, headers: HashMap<String, String> = HashMap()) : this("", body, headers)
 
     constructor(other: APIRequest<*>, newBody: T) {
