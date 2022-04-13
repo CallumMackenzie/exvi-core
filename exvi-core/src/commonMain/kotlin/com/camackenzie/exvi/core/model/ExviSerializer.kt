e90f81c1b698e0ff293201b816f4765c8a03050d
callum <alexxander1611@gmail.com>
2022-04-13 15:30:14 -0600

    Working on fixing serialization

--- exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/ExviSerializer.kt
+++ exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/ExviSerializer.kt
@@ -20,93 +20,82 @@ object ExviSerializer {
         coerceInputValues = true
         ignoreUnknownKeys = true
     }
-    val serializer: Json
 
-    init {
-        this.serializer = try {
-            Json {
-                serializersModule = Json.serializersModule + SerializersModule {
-                    polymorphic(ActiveExercise::class) {
-                        subclass(ActualActiveExercise::class)
-                    }
-                    polymorphic(Workout::class) {
-                        subclass(ActualWorkout::class)
-                    }
-                    polymorphic(SingleExerciseSet::class) {
-                        subclass(ActualSingleExerciseSet::class)
-                    }
-                    polymorphic(ExerciseSet::class) {
-                        subclass(ActualExerciseSet::class)
-                    }
-                    polymorphic(ActiveExercise::class) {
-                        subclass(ActualActiveExercise::class)
-                    }
-                    polymorphic(Exercise::class) {
-                        subclass(ActualExercise::class)
-                    }
-                    polymorphic(GenericDataRequest::class) {
-                        subclass(WorkoutPutRequest::class)
-                        subclass(WorkoutListRequest::class)
-                        subclass(ActiveWorkoutPutRequest::class)
-                        subclass(DeleteWorkoutsRequest::class)
-                        subclass(GetBodyStatsRequest::class)
-                        subclass(SetBodyStatsRequest::class)
-                        subclass(CompatibleVersionRequest::class)
-                        subclass(AccountCreationRequest::class)
-                        subclass(LoginRequest::class)
-                        subclass(RetrieveSaltRequest::class)
-                    }
-                    polymorphic(GenericDataResult::class) {
-                        subclass(NoneResult::class)
-                        subclass(WorkoutListResult::class)
-                        subclass(ActiveWorkoutListResult::class)
-                        subclass(BooleanResult::class)
-                        subclass(GetBodyStatsResponse::class)
-                        subclass(AccountSaltResult::class)
-                        subclass(AccountAccessKeyResult::class)
-                    }
-                    this@ExviSerializer.defaultJsonConfig(this@Json)
+    val serializer: Json = try {
+        Json {
+            serializersModule = Json.serializersModule + SerializersModule {
+                polymorphic(Exercise::class) {
+                    subclass(ActualExercise::class)
                 }
+                polymorphic(SingleExerciseSet::class) {
+                    subclass(ActualSingleExerciseSet::class)
+                }
+                polymorphic(ExerciseSet::class) {
+                    subclass(ActualExerciseSet::class)
+                }
+                polymorphic(ActiveExercise::class) {
+                    subclass(ActualActiveExercise::class)
+                }
+                polymorphic(Workout::class) {
+                    subclass(ActualWorkout::class)
+                }
+                polymorphic(ActiveWorkout::class) {
+                    subclass(ActualActiveWorkout::class)
+                }
+                polymorphic(GenericDataRequest::class) {
+                    subclass(WorkoutPutRequest::class)
+                    subclass(WorkoutListRequest::class)
+                    subclass(ActiveWorkoutPutRequest::class)
+                    subclass(DeleteWorkoutsRequest::class)
+                    subclass(GetBodyStatsRequest::class)
+                    subclass(SetBodyStatsRequest::class)
+                    subclass(CompatibleVersionRequest::class)
+                    subclass(AccountCreationRequest::class)
+                    subclass(LoginRequest::class)
+                    subclass(RetrieveSaltRequest::class)
+                }
+                polymorphic(GenericDataResult::class) {
+                    subclass(NoneResult::class)
+                    subclass(WorkoutListResult::class)
+                    subclass(ActiveWorkoutListResult::class)
+                    subclass(BooleanResult::class)
+                    subclass(GetBodyStatsResponse::class)
+                    subclass(AccountSaltResult::class)
+                    subclass(AccountAccessKeyResult::class)
+                }
+                this@ExviSerializer.defaultJsonConfig(this@Json)
             }
-        } catch (e: Throwable) {
-            ExviLogger.e(e, tag = "CORE") { "Could not initialize ExviSerializer" }
-            throw e
         }
+    } catch (e: Throwable) {
+        ExviLogger.e(e, tag = "CORE") { "Could not initialize ExviSerializer" }
+        Json { this.defaultJsonConfig() }
     }
 
     fun <T> toJson(serializer: SerializationStrategy<T>, value: T): String = try {
         ExviSerializer.serializer.encodeToString(serializer, value)
     } catch (ex: SerializationException) {
-        ExviLogger.e(ex, tag = "CORE") {
-            "PRIMARY SERIALIZATION FAILURE: Falling back to secondary serialization technique"
-        }
+        ExviLogger.e(ex, tag = "CORE") { "Primary serialization failure" }
         Json.encodeToString(serializer, value)
     }
 
     fun <T> fromJson(deserializer: DeserializationStrategy<T>, json: String): T = try {
         ExviSerializer.serializer.decodeFromString(deserializer, json)
     } catch (ex: SerializationException) {
-        ExviLogger.e(ex, tag = "CORE") {
-            "PRIMARY SERIALIZATION FAILURE: Falling back to secondary serialization technique"
-        }
+        ExviLogger.e(ex, tag = "CORE") { "Primary serialization failure" }
         Json.decodeFromString(deserializer, json)
     }
 
     inline fun <reified T> toJson(value: T): String = try {
         ExviSerializer.serializer.encodeToString(value)
     } catch (ex: SerializationException) {
-        ExviLogger.e(ex, tag = "CORE") {
-            "PRIMARY SERIALIZATION FAILURE: Falling back to secondary serialization technique"
-        }
+        ExviLogger.e(ex, tag = "CORE") { "Primary serialization failure" }
         Json.encodeToString(value)
     }
 
     inline fun <reified T> fromJson(json: String): T = try {
         ExviSerializer.serializer.decodeFromString(json)
     } catch (ex: SerializationException) {
-        ExviLogger.e(ex, tag = "CORE") {
-            "PRIMARY DESERIALIZATION FAILURE: Falling back to secondary deserialization technique"
-        }
+        ExviLogger.e(ex, tag = "CORE") { "Primary serialization failure" }
         Json.decodeFromString(json)
     }
 }
