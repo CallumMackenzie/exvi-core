ce95b9c541860e39d77aa3d696fc804c5193b3f9
Callum Mackenzie <Alexxander1611@gmail.com>
2022-04-14 17:26:53 -0600

    Make SelfSerializable interface less restrictive

--- exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/util/SelfSerializable.kt
+++ exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/util/SelfSerializable.kt
@@ -4,7 +4,7 @@ import com.camackenzie.exvi.core.model.ExviSerializer
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Transient
 
-interface SelfSerializable<T : SelfSerializable<T>> {
+interface SelfSerializable<T> {
     fun toJson(): String = ExviSerializer.toJson(serializer, this as T)
 
     @Transient
