687fbd3af263b5b90a3ab31b65c1d89e43c3cada
Callum Mackenzie <Alexxander1611@gmail.com>
2022-05-23 18:11:06 -0600

    Make friended users identifiable

--- exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/FriendedUser.kt
+++ exvi-core/src/commonMain/kotlin/com/camackenzie/exvi/core/model/FriendedUser.kt
@@ -5,6 +5,7 @@
 package com.camackenzie.exvi.core.model
 
 import com.camackenzie.exvi.core.util.EncodedStringCache
+import com.camackenzie.exvi.core.util.Identifiable
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -12,4 +13,6 @@ data class FriendedUser(
     val username: EncodedStringCache,
     val acceptedRequest: Boolean,
     val incomingRequest: Boolean,
-)
+) : Identifiable {
+    override fun getIdentifier(): EncodedStringCache = username
+}
